9542280185e99580800907fe5105a179f8e63f0e
VaishaalKrishnaVS <72886020+VaishaalKrishnaVS@users.noreply.github.com>
2021-02-03 19:16:22 +0530

    completed day 21
    completed the snake game

---
+++ snake game/snake.py
@@ -0,0 +1,53 @@
+from turtle import Turtle
+
+STARTING_POS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DIS = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for i in STARTING_POS:
+            self.add_segment(i)
+
+    def add_segment(self, i):
+        new_seg = Turtle("square")
+        new_seg.color("white")
+        new_seg.penup()
+        new_seg.goto(i)
+        self.segments.append(new_seg)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for s in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[s - 1].xcor()
+            new_y = self.segments[s - 1].ycor()
+            self.segments[s].goto(new_x, new_y)
+        self.head.forward(MOVE_DIS)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
